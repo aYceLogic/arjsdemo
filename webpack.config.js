1c367d02dde850072f036358fc017f5c4699d6b2
aYce Logic <95125160+aYceLogic@users.noreply.github.com>
2026-07-27 12:48:27 +0100

    webpack workflow update

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
